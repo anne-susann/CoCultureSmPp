001de8dfd15d72cdd9e7d967c92b84ad241749ba
Anne-Susann Abel <118827492+anne-susann@users.noreply.github.com>
2023-09-29 17:59:11 +0200

    final version of workflow
    pull request for MAW

--- MS1_workflow_XCMS/MS1_workflow.R
+++ MS1_workflow_XCMS/MS1_workflow.R
@@ -19,10 +19,15 @@ library(htmlwidgets)            # For creating html plots
 library(stringr)              
 
 ### MS1_workflow_functions
+# this is an examplary run of the workflow using the functions from the functions file
+# if you want to run the workflow on your own data edit the input parameters for the functions accordingly
+
+# edit for your local path to the functions file
 source("C:/Users/abela/Documents/GitHub/CoCultureSmPp/MS1_workflow_XCMS/MS1_workflow_functions.R")
 
 # ---- analysis for ENDO neg ----
 # defining variable for functions to use
+# edit files path to your local path to the files
 raw_data_MS1_ENDO_neg <- list.files("C:/Users/abela/Documents/Uni_Jena/Masterarbeit/MAW-Co-culture/MS1_pos_neg", pattern = "neg_ENDO", full.names = TRUE)
 phenodata <- read.csv("phenodata_ENDO_neg.csv")
 result_dir_name <- "ENDO_neg_testing_results"
@@ -32,61 +37,30 @@ ms1_params_ENDO_neg <- CentWaveParam(ppm=25, mzCenterFun="wMean", peakwidth=c(14
                                      integrate=1, firstBaselineCheck=TRUE, verboseColumns=FALSE, 
                                      fitgauss=FALSE, roiList=list(), roiScales=numeric())
 
-### trial run data_preparation
+# prepare the data 
 msd <- data_preparation(files = raw_data_MS1_ENDO_neg, phenodata = phenodata, result_dir_name = "ENDO_neg_testing_results", 
                         plots_dir_name = "ENDO_neg_testing_plots", chrom_run_sec = 700, msLevel = NULL)
-
-### trial run chromatogram
+# plot chromatogram
 chrom_msd <- chromatogram_qc(msd, phenodata, result_dir_name, plots_dir_name)
 
-### trial run peak_detection
+# perform peak detection on chromatographic peaks
 ms1_data <- peak_detection(msd, CentWaveParam = ms1_params_ENDO_neg, result_dir_name, plots_dir_name)
 
-### trial run grouping_1
+# grouping of the peaks
 ms1_data <- grouping_1(ms1_data, PeakDensityParam_gr = NULL, result_dir_name)
 
-### trial run rt_correction
+# perform retention time correction
 ms1_data <- rt_correction(ms1_data, plots_dir_name, result_dir_name, PeakGroupsParam_rt = NULL, chromas_msd = chrom_msd)
 
-### trial run grouping_2
+# second grouping of the peaks
 ms1_data <- grouping_2(ms1_data, PeakDensityParam_gr = NULL, result_dir_name)
 
-### trial run feature_extraction
+# create a feature list 
 feature_list <- feature_extraction(ms1_data, result_dir_name)
 
-### trial run feature_transformation
+# prepare the feature list for further analyses
 feature_list <- feature_transformation(feature_list, result_dir_name, plots_dir_name)
 
-### trial run bina_list_creation
+# create a binary absence/presence list for statistical analysis
 bina_list <- bina_list_creation(ms1_matrix = NULL, intensity_cutoff = 14, result_dir_name)
 
-# ---- analysis for ENDO pos ----
-raw_data_MS1_ENDO_pos <- list.files("C:/Users/abela/Documents/Uni_Jena/Masterarbeit/MAW-Co-culture/MS1_pos_neg", pattern = "pos_ENDO", full.names = TRUE)
-phenodata <- read.csv("phenodata_ENDO_pos.csv")
-result_dir_name <- "ENDO_pos_testing_results"
-plots_dir_name <- "ENDO_pos_testing_plots"
-ms1_params_ENDO_pos <- CentWaveParam(ppm=25, mzCenterFun="wMean", peakwidth=c(12, 51), 
-                                     prefilter=c(4, 60), mzdiff= 0.000099, snthresh=6, noise=0, 
-                                     integrate=1, firstBaselineCheck=TRUE, verboseColumns=FALSE, 
-                                     fitgauss=FALSE, roiList=list(), roiScales=numeric())
-
-
-msd <- data_preparation(files = raw_data_MS1_ENDO_pos, phenodata = phenodata, result_dir_name, plots_dir_name, chrom_run_sec = 700, msLevel = 1)
-
-chrom_msd <- chromatogram_qc(msd, phenodata, result_dir_name, plots_dir_name)
-
-ms1_data <- peak_detection(msd, CentWaveParam = ms1_params_ENDO_pos, result_dir_name, plots_dir_name)
-
-ms1_data <- grouping_1(ms1_data, PeakDensityParam_gr = NULL, result_dir_name)
-
-ms1_data <- rt_correction(ms1_data, plots_dir_name, result_dir_name, PeakGroupsParam_rt = NULL, chromas_msd = chrom_msd)
-
-ms1_data <- grouping_2(ms1_data, PeakDensityParam_gr = NULL, result_dir_name)
-
-feature_list <- feature_extraction(ms1_data, result_dir_name)
-
-feature_list <- feature_transformation(feature_list, result_dir_name, plots_dir_name)
-
-bina_list <- bina_list_creation(ms1_matrix = NULL, intensity_cutoff = 14, result_dir_name)
-
-# ---- Statistics on ENDO ----
